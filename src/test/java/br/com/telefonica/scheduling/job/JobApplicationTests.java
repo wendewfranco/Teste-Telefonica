db7a12c5f65645d90debc83f70b5c3e77572e7a0
wendewfranco <wendewfranco@gmail.com>
2022-01-04 20:24:02 -0300

    comite inicial

---
+++ src/test/java/br/com/telefonica/scheduling/job/JobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.telefonica.scheduling.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
